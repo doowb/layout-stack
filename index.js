78556aff876897634001573ad80ea0574c2f3fc8
Brian Woodward <brian.woodward@gmail.com>
2014-11-18 16:06:37 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * layout-stack <https://github.com/doowb/layout-stack>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
