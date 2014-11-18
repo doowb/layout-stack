234aead3e535a8e5cae08114e1b5c726a030581e
Brian Woodward <brian.woodward@gmail.com>
2014-11-18 16:27:47 -0500

    code, tests, and docs

--- index.js
+++ index.js
@@ -7,6 +7,51 @@
 
 'use strict';
 
-module.exports = function () {
-  // do stuff
-};
+/**
+ * Build a layout stack.
+ *
+ * ```js
+ * var stack = createStack('2-col-side-nav', layouts);
+ * ```
+ *
+ * @param  {String} `name` The name of the first layout to add to the stack.
+ * @param  {Object} `layouts` Possible layout templates tha could be used in the stack.
+ * @param  {Object} `options` Options to pass to `assertLayout`.
+ * @return {Array} The layout stack starting with the given name.
+ * @api public
+ */
+
+module.exports = function createStack (name, layouts, opts) {
+  opts = opts || {};
+  var template = {};
+  var stack = [];
+  var prev = null;
+
+  while (name && (prev !== name) && (template = layouts[name])) {
+    stack.unshift(name);
+    prev = name;
+    name = assertLayout(template.layout, opts.defaultLayout);
+  }
+  return stack;
+}
+
+/**
+ * Assert whether or not a layout should be used based on
+ * the given `value`. If a layout should be used, the name of the
+ * layout is returned, if not `null` is returned.
+ *
+ * @param  {*} `value`
+ * @return {String|Null} Returns `true` or `null`.
+ * @api private
+ */
+
+function assertLayout(value, defaultLayout) {
+  var isFalsey = require('falsey');
+  if (value === false || (value && isFalsey(value))) {
+    return null;
+  } else if (!value || value === true) {
+    return defaultLayout || null;
+  } else {
+    return value;
+  }
+}
